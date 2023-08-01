09dae6cdae7e7d2d0d7d7cdae1c649301947e20d
masbagas23 <hasea23@gmail.com>
2023-08-01 17:07:54 +0700

    update query statistic

--- app/Http/Controllers/Api/StatisticController.php
+++ app/Http/Controllers/Api/StatisticController.php
@@ -15,16 +15,14 @@ class StatisticController extends Controller
 {
     public function topCustomerTrip()
     {
-        $customers = Customer::with(['applications:id,customer_id'])->whereHas('applications', function($query){
-            $date = Carbon::parse(request()->filter_month);
-            $query->whereMonth('start_date', $date)->whereYear('start_date', $date)->where('status', BusinessTripApplication::STATUS_APPROVE);
-        })->get();
+        $customers = Customer::with(['applications:id,customer_id'])->whereHas('applications')->get();
         $data = [];
+        $date = Carbon::parse(request()->filter_month);
         foreach ($customers as $customer) {
             // $payload['y'] = $customer->applications->sum(function ($application) {
             //     return $application->expense ? $application->expense->total_nominal : 0;
             // });
-            $payload['y'] = $customer->applications->count();
+            $payload['y'] = $customer->applications()->whereMonth('start_date', $date)->whereYear('start_date', $date)->where('status', BusinessTripApplication::STATUS_APPROVE)->count();
             $payload['x'] = $customer['name'];
             array_push($data, $payload);
         }
@@ -36,13 +34,11 @@ class StatisticController extends Controller
 
     public function topCustomerCost()
     {
-        $customers = Customer::with(['applications:id,customer_id', 'applications.expense:id,application_id,total_nominal'])->whereHas('applications', function($query){
-            $date = Carbon::parse(request()->filter_month);
-            $query->whereMonth('start_date', $date)->whereYear('start_date', $date)->where('status', BusinessTripApplication::STATUS_APPROVE);
-        })->whereHas('applications.expense')->get();
+        $customers = Customer::with(['applications:id,customer_id', 'applications.expense:id,application_id,total_nominal'])->whereHas('applications.expense')->get();
         $data = [];
-        foreach ($customers as $customer) {
-            $payload['y'] = $customer->applications->sum(function ($application) {
+        $date = Carbon::parse(request()->filter_month);
+        foreach ($customers as $customer) {           
+            $payload['y'] = $customer->applications()->whereMonth('start_date', $date)->whereYear('start_date', $date)->where('status', BusinessTripApplication::STATUS_APPROVE)->sum(function ($application) {
                 return $application->expense ? $application->expense->total_nominal : 0;
             });
             $payload['x'] = $customer['name'];
